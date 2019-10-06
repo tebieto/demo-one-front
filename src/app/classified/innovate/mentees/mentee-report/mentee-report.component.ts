351c1b868e7dccb4ce60031574593d9afba3ff76
Terry Ebieto <tebieto@gmail.com>
2019-10-06 18:18:16 +0100

    06-10-2019 third

---
+++ src/app/classified/innovate/mentees/mentee-report/mentee-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mentee-report',
+  templateUrl: './mentee-report.component.html',
+  styleUrls: ['./mentee-report.component.css']
+})
+export class MenteeReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
